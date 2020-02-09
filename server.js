facf740a7d9c6cd1604c63c85c7132fcedf20696
igilfu <igilfu@gmail.com>
2020-02-09 02:16:16 -0500

    update server

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.use(methodOverride('_method'))
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+    next()
+})
+
+
+
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL,{
    useNewUrlParser:true,
